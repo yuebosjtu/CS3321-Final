1ebf47c2da8a8a77499ff662872320f87ce18f11
Ma Yuebo <158435258+yuebosjtu@users.noreply.github.com>
2025-05-18 08:32:13 +0000

    1

---
+++ frontend/vite.config.js
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 5173, // 开发服务器端口
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000', // 后端 API 地址
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/styles/variables.scss" as *;`
+      }
+    }
+  }
+})
